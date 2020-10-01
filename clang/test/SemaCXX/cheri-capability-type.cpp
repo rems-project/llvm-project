56037e3bce606f8b18359b9f00541f4129a38cbc
Alex Richardson <Alexander.Richardson@cl.cam.ac.uk>
2020-10-01 15:41:14 +0100

    Rename test and add a comment explaining what is being tested

--- clang/test/SemaCXX/cheri-capability-type.cpp
+++
@@ -1,9 +0,0 @@
-// RUN: %clang_cc1 -triple cheri-unknown-freebsd -fsyntax-only -verify %s
-
-// expected-no-diagnostics
-int f() {
-   void * __capability c1;
-   void * __capability c2;
-   if (c1 == c2) return f();
-   return 0;
-}
