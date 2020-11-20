38fd5761bd41d7250966d8d645d5f0fa2419948a
Diana Picus <diana.picus@linaro.org>
2020-11-20 03:08:59 +0000

    [Morello] Disable expr command in lldb for AArch64
    The 'expr' command is currently not supported because we do not have
    ptrace support for restoring the capability registers after code
    injection. Without that, running 'expr' just clears the tags and/or
    corrupts the top bits of the capability registers, thus interfering with
    the whole debug session in unexpected ways. It is better to disable it
    for now.
    
    Note that this also disables the 'print' command, which is really just
    an alias for 'expr'.

--- lldb/include/lldb/Target/Target.h
+++ lldb/include/lldb/Target/Target.h
@@ -80,6 +80,8 @@ public:
 
   bool GetMoveToNearestCode() const;
 
+  bool GetForceExpressionEvaluation() const;
+
   lldb::DynamicValueType GetPreferDynamicValue() const;
 
   bool SetPreferDynamicValue(lldb::DynamicValueType d);
