9890206603f9757b7c3ee626acf2de97a33efcda
Jessica Clarke <jrtc27@jrtc27.com>
2022-07-13 19:42:46 +0100

    [lld][Morello] Add a test demonstrating broken 64-bit PCC handling

---
+++ lld/test/ELF/morello/morello-capreloc-pcc-64bit.s
@@ -0,0 +1,23 @@
+// REQUIRES: aarch64
+// RUN: llvm-mc -filetype=obj -triple=aarch64 %s -o %t.o -mattr=+morello,+c64 -target-abi purecap
+// RUN: ld.lld --image-base 0x100000000 %t.o -o %t
+// RUN: llvm-readobj --cap-relocs %t | FileCheck %s
+
+/// Check that we can correctly handle PCC's range extending past 2^32
+/// FIXME: This is currently broken
+
+	.text
+	.type code_sym, %function
+code_sym:
+	ret
+	.size code_sym, . - code_sym
+
+	.data
+	.type code_cap, %object
+code_cap:
+	.chericap code_sym
+	.size code_cap, . - code_cap
+
+// CHECK:      CHERI __cap_relocs [
+// CHECK-NEXT:   0x100030200 (code_cap) Base: 0x101c8 (<unknown symbol>+4294967297) Length: 4 Perms: (FUNC)
+// CHECK-NEXT: ]
