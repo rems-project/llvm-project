6cd30aec13125e813f827d78a3fca3359e6c291e
Irina Dudina <irina.dudina@ed.ac.uk>
2023-09-08 13:54:51 +0100

    [CHERI_CSA] Enable cheri.* checkers by default on purecap

--- clang/lib/Driver/ToolChains/Arch/AArch64.h
+++ clang/lib/Driver/ToolChains/Arch/AArch64.h
@@ -33,7 +33,7 @@ void getMorelloMode(const Driver &D, const llvm::Triple &Triple,
                     bool &C64, bool &PureCap,
                     bool &ReducedCapRegs);
 
-bool isPurecap(const llvm::opt::ArgList &Args, llvm::Triple &Triple);
+bool isPurecap(const llvm::opt::ArgList &Args, const llvm::Triple &Triple);
 
 } // end namespace aarch64
 } // end namespace target
