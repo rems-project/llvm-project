412e38c0ae8b03578e9da30a4e0069aee95958df
Irina Dudina <irina.dudina@ed.ac.uk>
2023-12-08 18:16:46 +0000

    [CHERI_CSA] PointerAlignmentChecker: fix FP for adjacent objects
    Do not propagate CapStorage attribute to shifted pointer values.
    This suppressed the false positive warning for adjacent objects
    of distinct types

--- clang/lib/StaticAnalyzer/Checkers/PointerAlignmentChecker.cpp
+++ clang/lib/StaticAnalyzer/Checkers/PointerAlignmentChecker.cpp
@@ -570,6 +570,18 @@ void PointerAlignmentChecker::checkPreCall(const CallEvent &Call,
   }
 }
 
+namespace {
+
+bool isNonZeroShift(const SVal& V) {
+  if (const MemRegion *MR = V.getAsRegion())
+    if (auto *ER = MR->getAs<ElementRegion>())
+      if (!ER->getIndex().isZeroConstant())
+          return true;
+  return false;
+}
+
+} // namespace
+
 void PointerAlignmentChecker::checkPostStmt(const CastExpr *CE,
                                             CheckerContext &C) const {
   CastKind CK = CE->getCastKind();
@@ -616,13 +628,15 @@ void PointerAlignmentChecker::checkPostStmt(const CastExpr *CE,
 
   /* Update CapStorageSet */
   const ProgramPointTag *Tag = nullptr;
-  if ((isCapabilityStorage(SrcVal, State, ASTCtx) || DstIsCapStorage)
-      && !isCapabilityStorage(DstVal, State, ASTCtx)) {
-    if (const MemRegion *R = DstVal.getAsRegion()) {
+  if (!isCapabilityStorage(DstVal, State, ASTCtx)) {
+    if ((isCapabilityStorage(SrcVal, State, ASTCtx) && !isNonZeroShift(SrcVal))
+        || DstIsCapStorage) {
+        if (const MemRegion *R = DstVal.getAsRegion()) {
         State = State->add<CapStorageSet>(R->StripCasts());
         Updated = true;
         if (DstIsCapStorage)
           Tag = C.getNoteTag("Cast to capability-containing type");
+        }
     }
   }
 
