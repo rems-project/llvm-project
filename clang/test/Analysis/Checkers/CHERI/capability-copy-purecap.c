fdc976208271179a09c33d0f65dd199b1471e060
Irina Dudina <irina.dudina@ed.ac.uk>
2024-03-20 16:27:37 +0000

    [CHERI_CSA] CapabilityCopyChecker: ReportForCharPtr=false by default

--- clang/test/Analysis/Checkers/CHERI/capability-copy-purecap.c
+++ clang/test/Analysis/Checkers/CHERI/capability-copy-purecap.c
@@ -1,6 +1,8 @@
-// RUN: %cheri_purecap_cc1 -analyze -analyzer-checker=core,cheri.CapabilityCopy -verify %s
+// RUN: %cheri_purecap_cc1 -analyze -analyzer-checker=core,cheri.CapabilityCopy \
+// RUN:                    -analyzer-config cheri.CapabilityCopy:ReportForCharPtr=true -verify %s
 // RUN: %clang_cc1 -triple aarch64-none-elf -target-feature +morello -target-feature +c64 -target-abi purecap \
-// RUN:            -analyze -analyzer-checker=core,cheri.CapabilityCopy -verify %s
+// RUN:            -analyze -analyzer-checker=core,cheri.CapabilityCopy \
+// RUN:            -analyzer-config cheri.CapabilityCopy:ReportForCharPtr=true -verify %s
 
 typedef __intcap_t intptr_t;
 typedef __uintcap_t uintptr_t;
